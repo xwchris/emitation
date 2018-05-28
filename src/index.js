bc073f453c744326b201e6d2c97eab8a7a2c91d3
zhangxiaowei <zhangxiaowei@innobuddy.com>
2018-05-28 20:21:20 +0800

    fixed untracked files

--- src/index.js
+++
@@ -1,8 +0,0 @@
-const TemplateEngine = require('./template');
-
-// var template = '<p>Hello, my name is <%name%>. I\'m <%age%> years old.</p>';
-var template = 'My skills:' +
-'<%for(var index in this.skills) {%>' +
-'<a href=""><%this.skills[index]%></a>' +
-'<%}%>';
-console.log(TemplateEngine(template, {skills: ['js', 'css', 'html']}));
